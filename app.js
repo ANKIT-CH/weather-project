29595a6c684eae0183a2c46db169e24bc2e5db22
ANKIT-CH <ach1134544@gmail.com>
2021-12-02 13:38:45 +0530

    feching weather data from openWeatherAPI and passing it to our website successfully

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const https = require("https");
+
+const app = express();
+
+app.get("/", function (req, res) {
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=37922baade64217f856db97f50f973ad&units=metric";
+  https.get(url, function (weatherResp) {
+    console.log(weatherResp.statusCode);
+
+    weatherResp.on("data", function (data) {
+      const weatherData = JSON.parse(data);
+      //   console.log(weatherData);
+      res.write("<p>weather is " + weatherData.weather[0].description + "</p>");
+
+      const iconId = weatherData.weather[0].icon;
+      const iconUrl = "//openweathermap.org/img/wn/" + iconId + "@2x.png";
+      res.write("<img src=" + iconUrl + ">");
+      http: res.send();
+    });
+
+    weatherResp.on("error", function (error) {
+      console.log(error);
+      res.send(error);
+    });
+  });
+});
+
+app.listen(3000, function () {
+  console.log("server running on port 3000");
+});
